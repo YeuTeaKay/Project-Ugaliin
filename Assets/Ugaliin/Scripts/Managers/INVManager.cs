2274bfc17ae9c336dd0edf66d82515b2de68d0b4
UTK <115647767+YeuTeaKay@users.noreply.github.com>
2024-04-30 18:02:49 +0800

    Finishing up the Inventory system and Added Items
    Still not finish system but at night I will probably

---
+++ Assets/Ugaliin/Scripts/Managers/INVManager.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;   
+
+public class INVManager : MonoBehaviour
+{
+    private static INVManager instance;
+    public List<Item> inventory = new List<Item>();
+
+    public Transform ItemContent;
+    public GameObject InventoryItem;
+
+    private void Awake()
+    {
+
+        if (instance != null)
+        {
+            Debug.LogError("Found more than one Input Manager in the scene.");
+        }
+        instance = this;
+    }
+    
+    public void AddItem(Item item)
+    {
+        inventory.Add(item);
+    }
+
+    public void RemoveItem(Item item)
+    {
+        inventory.Remove(item);
+    }
+
+    public void ListItems()
+    {
+        foreach (var item in inventory)
+        {
+            GameObject obj = Instantiate(InventoryItem, ItemContent);
+            var itemName = obj.transform.Find("ItemName").GetComponent<TextMeshProUGUI>();
+            var itemIcon = obj.transform.Find("ItemIcon").GetComponent<Image>();
+
+            itemName.text = item.ItemName;
+            itemIcon.sprite = item.Icon;
+        }
+    }
+}
