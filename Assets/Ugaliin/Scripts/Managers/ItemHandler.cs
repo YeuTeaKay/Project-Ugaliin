eef6211b411010ddd6da2af277462c226d0d1720
UTK <115647767+YeuTeaKay@users.noreply.github.com>
2024-05-04 00:53:12 +0800

    Slight Improvement
    Able to connect the INV manager to the other manager

--- Assets/Ugaliin/Scripts/Managers/ItemHandler.cs
+++ Assets/Ugaliin/Scripts/Managers/ItemHandler.cs
@@ -7,34 +7,47 @@ public class ItemHandler : MonoBehaviour
     [SerializeField] private Item choiceItem1;
     [SerializeField] private Item choiceItem2;
 
+    private bool actionPerformed = false;
+
 
     private void Update()
     {
-        string IsplayerChoice = ((Ink.Runtime.StringValue) VNManager
+        if (!actionPerformed)
+        {
+            string IsplayerChoice = ((Ink.Runtime.StringValue) VNManager
             .GetInstance()
             .GetVariableState("playerChoice")).value;
 
-        switch (IsplayerChoice)
-        {
-            case "choiceItem0":
-                Debug.Log("player choice: " + IsplayerChoice);
-                break;
-            case "choiceItem1":
-                Debug.Log("player choice: " + IsplayerChoice);
-                break;
-            case "choiceItem2":
-                Debug.Log("player choice: " + IsplayerChoice);
-                break;
-            default:
-                Debug.LogWarning("Player Choice was not found: " + IsplayerChoice);
-                break;
+            switch (IsplayerChoice)
+            {
+                case "choiceItem0":
+                    Debug.Log("player choice: " + IsplayerChoice);
+                    GiveItem(choiceItem0);
+                    actionPerformed = true;
+                    break;
+                case "choiceItem1":
+                    Debug.Log("player choice: " + IsplayerChoice);
+                    GiveItem(choiceItem1);
+                    actionPerformed = true;
+                    break;
+                case "choiceItem2":
+                    Debug.Log("player choice: " + IsplayerChoice);
+                    GiveItem(choiceItem2);
+                    actionPerformed = true;
+                    break;
+                default:
+                    Debug.LogWarning("Player Choice was not found: " + IsplayerChoice);
+                    break;
+            }
         }
     }
 
-    private void GiveItem()
+    private void GiveItem(Item item)
     {
-        //INVManager.GetInstance().AddItem(otherItem);
-
+        INVManager.GetInstance().AddItem(item);
     }
+
+
+
 
 }
